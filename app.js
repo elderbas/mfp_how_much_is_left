77c972f4259adac2586a45cf98dfd5e709ced2c2
Brian Schermerhorn <bscherm@Brians-MacBook-Pro.local>
2018-09-09 22:35:12 -0700

    options for different end day type, and by week calc fixed

--- app.js
+++ app.js
@@ -8,24 +8,47 @@ const fs = require('fs');
 
 const config = require('./config')
 
-const yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
-const DAILY_CALORIE_LIMIT = 1800;
 const cheatDays = config.cheatDays || [];
+
+
+let endDate;
+switch(config.endPeriodDefault) {
+  case 'yesterday': {
+    endDate = moment().subtract(1, 'day');
+    break;
+  }
+  case 'today': {
+    endDate = moment();
+    break;
+  }
+  case 'week': {
+    endDate = moment().endOf('week');
+    break;
+  }
+}
 
 getCalsFromPreviousDays()
 
 function getCalsFromPreviousDays() {
-
-
   mfp.fetchDateRange(
     config.userName,
     config.startDate,
-    config.endDate || yesterday,
+    endDate.format('YYYY-MM-DD'),
     ['calories'],
     function(data) {
       const calsRolledOver = getCaloriesRolledOver(data.data);
-      logOutput(calsRolledOver);
+      const result = calsRolledOver + addCalsForFutureDays();
+      logOutput(result);
   });
+}
+
+function addCalsForFutureDays() {
+  const today = moment();
+  if (config.endPeriodDefault === 'week' && (endDate > today)) {
+    const numDaysToAddFor = endDate.diff(today, 'days');
+    return -(numDaysToAddFor * config.dailyCalorieLimit);
+  }
+  return 0;
 }
 
 function getCaloriesRolledOver(data) {
@@ -40,11 +63,40 @@ function getCaloriesRolledOver(data) {
 }
 
 function logOutput(calsRolledOver) {
-  if (calsRolledOver < 0) {
-    console.log(`You have ${c.green(Math.abs(calsRolledOver))} calories available to use from previous days`); // eslint-disable-line
-  }
-  else {
-    console.log(`You owe ${c.red.bgWhite(calsRolledOver)} calories from previous days`); // eslint-disable-line
+  const isNegative = calsRolledOver < 0;
+  const isNegativeNumberFormated = c.green(Math.abs(calsRolledOver));
+  const isPositiveNumberFormated = c.red.bgWhite(calsRolledOver);
+  switch(config.endPeriodDefault {
+    case 'yesterday': {
+      if (isNegative) {
+        console.log(`You have ${isNegativeNumberFormated} calories available to use from previous days`); // eslint-disable-line
+      }
+      else {
+        console.log(`You owe ${isPositiveNumberFormated} calories from previous days`); // eslint-disable-line
+      }
+      break;
+    }
+    case 'today': {
+      if (isNegative) {
+        console.log(`You have ${isNegativeNumberFormated} calories available to use until end of today`); // eslint-disable-line
+      }
+      else {
+        console.log(`You owe ${isPositiveNumberFormated} calories up until tonight.`); // eslint-disable-line
+      }
+      break;
+    }
+    case 'week': {
+      if (isNegative) {
+        console.log(`You have ${isNegativeNumberFormated} calories available to use until end of week`); // eslint-disable-line
+      }
+      else {
+        console.log(`You owe ${isPositiveNumberFormated} calories up until end of week`); // eslint-disable-line
+      }
+      break;
+    }
+    default: {
+      console.log('Error with end period type'); // eslint-disable-line
+    }
   }
 }
 
--- config.js
+++ config.js
@@ -3,6 +3,10 @@
 module.exports = {
   startDate: '2018-09-01',
   userName: 'elderbas',
+  
+  // 'yesterday' or 'week' or 'today'
+  endPeriodDefault: 'today',
+  
   dailyCalorieLimit: 1800,
   cheatDays: [
     // add more cheatdays to this array
